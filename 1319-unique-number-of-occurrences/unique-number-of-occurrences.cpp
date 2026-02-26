50f11c48f854465224132c476d2e80d49f05d0a3
hemendrawork07-source <hemendrawork.07@gmail.com>
2026-02-26 23:29:14 +0530

    Time: 2 ms (20.90%) | Memory: 12.1 MB (32.17%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]]++;
+        }
+        unordered_set<int> s;
+        for(auto x:m){
+            int feq=x.second;
+        if(s.find(feq)!=s.end()) return false;
+        else s.insert(feq);
+        }
+        return true;
+    }
+};
