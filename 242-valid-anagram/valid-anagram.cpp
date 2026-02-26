864eee50caff62de67522fb0dae34786abe8b74d
hemendrawork07-source <hemendrawork.07@gmail.com>
2026-02-26 22:00:22 +0530

    Time: 6 ms (23.69%) | Memory: 10 MB (10.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
-    }
+        if(s.length()!=t.length()) return false ;
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        for(int i=0;i<s.length();i++){
+            m1[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            m2[t[i]]++;
+        }
+        for(auto i:m1){
+            char ch1=i.first;
+            int freq1=i.second;
+            if(m2.find(ch1)!=m2.end()){
+                int freq2=m2[ch1];
+                if(freq1!=freq2) return false;
+            }else return false ;
+        }
+        return true;
+     }
 };
